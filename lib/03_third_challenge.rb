f69a89127827131ab166c5254ce60c375ffea6a4
Ryan Werner <rmwerner@umich.edu>
2020-06-17 01:50:15 +0000

    Automatically backed up by Learn

--- lib/03_third_challenge.rb
+++ lib/03_third_challenge.rb
@@ -1,11 +1,11 @@
 def third_challenge
   epic_tragedy = {
    :montague => {
-      :patriarch => {name: "Lord Montegue",
+      :patriarch => {name: "Lord Montague",
         age: "53"
         },
       :matriarch => {
-        name: "Lady Montegue",
+        name: "Lady Montague",
         age: "54"
         },
       :hero => {
