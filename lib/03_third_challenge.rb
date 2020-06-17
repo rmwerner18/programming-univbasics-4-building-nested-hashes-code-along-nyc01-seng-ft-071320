a2041578a4f3177fb2640d04f84613b907d3cd6e
Ryan Werner <rmwerner@umich.edu>
2020-06-17 01:38:19 +0000

    Automatically backed up by Learn

--- lib/03_third_challenge.rb
+++ lib/03_third_challenge.rb
@@ -5,10 +5,13 @@ def third_challenge
         age: "53"
         },
       :matriarch => {
-        #your key/value pairs here
+        name: "Lady Montegue"
+        age: "54"
         },
       :hero => {
-        #your key/value pairs here
+        name: "Romeo"
+        age: "15"
+        status: "alive"
         },
       :hero_friends => []
    },
