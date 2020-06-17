d83af25f44b0a64090e2846c4d5fecc8991462aa
Ryan Werner <rmwerner@umich.edu>
2020-06-17 01:47:48 +0000

    Automatically backed up by Learn

--- lib/04_fourth_challenge.rb
+++ lib/04_fourth_challenge.rb
@@ -23,13 +23,13 @@ def fourth_challenge
       :heroine => {name: "Juliet", age: "15", status: "alive"},
       :heroine_friends => [
         {
-          name: "Steven"
-          age: "30"
+          name: "Steven",
+          age: "30",
           attitude: "confused"
       },
       {
-        name: "Nurse"
-        age: "44"
+        name: "Nurse",
+        age: "44",
         attitude: "worried"
       }
       ]
